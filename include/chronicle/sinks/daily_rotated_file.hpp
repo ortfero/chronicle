0bcf32069ed95b1d8caae4c9b13103f40348fc9b
ortfero <ortfero@gmail.com>
2022-03-23 22:15:40 +0200

    Port daily_rotated_file to linux

--- include/chronicle/sinks/daily_rotated_file.hpp
+++ include/chronicle/sinks/daily_rotated_file.hpp
@@ -30,39 +30,42 @@
 #include <handleapi.h>
 #include <sysinfoapi.h>
 
-#endif
+#elif defined(__linux__)
 
+#include <unistd.h>
+#include <errno.h>
+
+#endif
 
 #include <date/date.h>
+#include <ufmt/text.hpp>
 
-
-#ifdef CHRONICLE_USE_SYSTEM_UFORMAT
-
-#include <uformat/texter.hpp>
-
-#else
-
-#include "../bundled/uformat/texter.hpp"
-
-#endif
-
-
-#include "../sink.hpp"
+#include <chronicle/sink.hpp>
 
 
 namespace chronicle::sinks {
 
 
-#if defined(_WIN32)
-
   class daily_rotated_file: public sink {
+#if defined(_WIN32)
+    using handle_type = void*;
+    static constexpr auto invalid_handle = nullptr;
+#elif defined(__linux__)
+    using handle_type = int;
+    static constexpr auto invalid_handle = -1;
+#endif
+
+    handle_type handle_{invalid_handle};
+    std::filesystem::path directory_;
+    std::filesystem::path base_name_;
+    std::filesystem::path extension_;
+    std::filesystem::path file_path_;
+    uint64_t log_day_{0};
+    unsigned part_{1};
+    std::size_t limit_{0};
+    std::size_t written_{0};
+    
   public:
-
-    static constexpr auto file_append_data = DWORD(0x0004);
-    static constexpr auto file_share_read = DWORD(0x00000001);
-    static constexpr auto open_always = DWORD(4);
-    static constexpr auto file_attribute_normal = DWORD(0x00000080);
-
 
     static std::unique_ptr<daily_rotated_file> open(std::filesystem::path const& path,
                                                     std::error_code& ec,
@@ -81,6 +84,109 @@ namespace chronicle::sinks {
     std::filesystem::path const& file_path() const noexcept { return file_path_; }
 
 
+    daily_rotated_file(daily_rotated_file&& other) noexcept:
+      handle_{ other.handle_ },
+      directory_{ std::move(other.directory_) },
+      base_name_{ std::move(other.base_name_) },
+      extension_{ std::move(other.extension_) },
+      file_path_{ std::move(other.file_path_) },
+      log_day_{ other.log_day_ },
+      part_{ other.part_ },
+      limit_{ other.limit_ },
+      written_{ other.written_ } {
+      other.handle_ = invalid_handle;
+    }
+
+
+    daily_rotated_file& operator = (daily_rotated_file&& other) noexcept {
+      if(handle_ != invalid_handle)
+#if defined(_WIN32)
+        CloseHandle(handle_);
+#elif defined(__linux__)
+        ::close(handle_);
+#endif
+      handle_ = other.handle_; other.handle_ = nullptr;
+      directory_ = std::move(other.directory_);
+      base_name_ = std::move(other.base_name_);
+      extension_ = std::move(other.extension_);
+      file_path_ = std::move(other.file_path_);
+      log_day_ = other.log_day_;
+      part_ = other.part_;
+      limit_ = other.limit_;
+      written_ = other.written_;
+      return *this;
+    }
+    
+    
+    bool ready() const noexcept override {
+      return handle_ != invalid_handle;
+    }
+
+
+    void write(time_point const& tp, char const* data, size_t size) noexcept override {
+      using namespace std::chrono;
+      std::error_code ec;
+      auto const now_day = uint64_t(duration_cast<hours>(tp.time_since_epoch()).count() / 24);
+      if(now_day != log_day_) {
+        log_day_ = now_day;
+        part_ = 1;
+        rotate_file(tp);
+      } else if(limit_ != 0 && written_ + size > limit_) {
+        ++part_;
+        rotate_file(tp, ec);
+      }
+      
+#if defined(_WIN32)
+      WriteFile(handle_, data, DWORD(size), nullptr, nullptr);
+#elif defined(__linux__)
+      ::write(handle_, data, size);
+#endif
+
+      written_ += size;
+    }
+    
+    
+    void flush() noexcept override {
+#if defined(_WIN32)
+        FlushFileBuffers(handle_);
+#elif
+        ::fdatasync(handle_);
+#endif
+    }
+
+
+    void close() noexcept override {
+      if(handle_ == invalid_handle)
+        return;
+#if defined(_WIN32)
+      CloseHandle(handle_);
+#elif defined(__linux__)
+      ::close(handle_);
+#endif
+      handle_ = invalid_handle;
+    }
+
+
+    void prologue(const char *data, size_t size) noexcept override {
+#if defined(_WIN32)
+      WriteFile(handle_, data, DWORD(size), nullptr, nullptr);
+#elif defined(__linux__)
+      ::write(handle_, data, size);
+#endif
+    }
+
+
+    void epilogue(const char *data, size_t size) noexcept override {
+#if defined(_WIN32)
+      WriteFile(handle_, data, DWORD(size), nullptr, nullptr);
+#elif defined(__linux__)
+      ::write(handle_, data, size);
+#endif
+    }
+
+
+  private:
+    
     daily_rotated_file(std::filesystem::path const& path,
                        std::size_t limit,
                        std::error_code& ec) noexcept {
@@ -96,100 +202,11 @@ namespace chronicle::sinks {
       auto const now = system_clock::now();
       log_day_ = duration_cast<hours>(now.time_since_epoch()).count() / 24;
       limit_ = limit;
-
-      rotate_file(now);
+      rotate_file(now, ec);
     }
 
 
-    daily_rotated_file(daily_rotated_file&& other) noexcept:
-      handle_{ other.handle_ },
-      directory_{ std::move(other.directory_) },
-      base_name_{ std::move(other.base_name_) },
-      extension_{ std::move(other.extension_) },
-      file_path_{ std::move(other.file_path_) },
-      log_day_{ other.log_day_ },
-      part_{ other.part_ },
-      limit_{ other.limit_ },
-      written_{ other.written_ } {
-      other.handle_ = nullptr;
-    }
-
-
-    daily_rotated_file& operator = (daily_rotated_file&& other) noexcept {
-      if(handle_ != nullptr)
-        CloseHandle(handle_);
-      handle_ = other.handle_; other.handle_ = nullptr;
-      directory_ = std::move(other.directory_);
-      base_name_ = std::move(other.base_name_);
-      extension_ = std::move(other.extension_);
-      file_path_ = std::move(other.file_path_);
-      log_day_ = other.log_day_;
-      part_ = other.part_;
-      limit_ = other.limit_;
-      written_ = other.written_;
-      return *this;
-    }
-    
-    
-    bool ready() const noexcept override {
-      return handle_ != nullptr;
-    }
-
-
-    void write(time_point const& tp, char const* data, size_t size) noexcept override {
-      using namespace std::chrono;
-      auto const now_day = uint64_t(duration_cast<hours>(tp.time_since_epoch()).count() / 24);
-      if(now_day != log_day_) {
-        log_day_ = now_day;
-        part_ = 1;
-        rotate_file(tp);
-      } else if(limit_ != 0 && written_ + size > limit_) {
-        ++part_;
-        rotate_file(tp);
-      }
-      
-      WriteFile(handle_, data, DWORD(size), nullptr, nullptr);
-      written_ += size;
-    }
-    
-    
-    void flush() noexcept override {
-      FlushFileBuffers(handle_);
-    }
-
-
-    void close() noexcept override {
-      if(handle_ == nullptr)
-        return;
-      CloseHandle(handle_);
-      handle_ = nullptr;
-    }
-
-
-    void prologue(const char *data, size_t size) noexcept override {
-      WriteFile(handle_, data, DWORD(size), nullptr, nullptr);
-    }
-
-
-    void epilogue(const char *data, size_t size) noexcept override {
-      WriteFile(handle_, data, DWORD(size), nullptr, nullptr);
-    }
-
-
-  private:
-
-    void* handle_{nullptr};
-    std::filesystem::path directory_;
-    std::filesystem::path base_name_;
-    std::filesystem::path extension_;
-    std::filesystem::path file_path_;
-    uint64_t log_day_{0};
-    unsigned part_{1};
-    std::size_t limit_{0};
-    std::size_t written_{0};
-
-
-    void rotate_file(std::chrono::system_clock::time_point tp) {
+    void rotate_file(std::chrono::system_clock::time_point tp, std::error_code& ec) {
       if(handle_ != nullptr) {
         CloseHandle(handle_);
         handle_ = nullptr;
@@ -206,8 +223,21 @@ namespace chronicle::sinks {
       file_path_ /= base_name_;
       file_path_ += texter.string().data();
       file_path_ += extension_;
+#if defined(_WIN32)
+    static constexpr auto file_append_data = DWORD(0x0004);
+    static constexpr auto file_share_read = DWORD(0x00000001);
+    static constexpr auto open_always = DWORD(4);
+    static constexpr auto file_attribute_normal = DWORD(0x00000080);
+    
       handle_ = CreateFileW(file_path_.native().data(), file_append_data, file_share_read, nullptr,
                                     open_always, file_attribute_normal, nullptr);
+      if(handle_ == nullptr)
+        ec = {int(GetLastError()), std::system_category()};
+#elif defined(__linux__)
+      handle_ = ::open(path.native().data(), O_WRONLY | O_CREAT | O_APPEND | S_IWUSR | S_IROTH);
+      if(handle_ == -1)
+        ec = {errno, std::system_category()};
+#endif
       written_ = 0;
     }
 
