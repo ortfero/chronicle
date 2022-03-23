e8fe1251e386f6b860b754293a435cbd057591e7
ortfero <ortfero@gmail.com>
2022-03-23 22:14:41 +0200

    Port std error sink to linux

--- include/chronicle/sinks/conerr.hpp
+++ include/chronicle/sinks/conerr.hpp
@@ -26,24 +26,45 @@
 #include <ProcessEnv.h>
 #include <fileapi.h>
 
+#elif defined(__linux__)
+
+#include <unistd.h>
+
+#else
+
+#error Unsupported system
+
 #endif
 
 
-#include "../sink.hpp"
+#include <chronicle/sink.hpp>
 
 
 namespace chronicle::sinks {
 
 
-#if defined(_WIN32)
+
 
   class conerr: public sink {
+      
+#if defined(_WIN32)
+    using handle_type = void*;
+    static constexpr auto invalid_handle = nullptr;
+#elif defined(__linux__)
+    using handle_type = int;
+    static constexpr auto invalid_handle = -1;
+#endif
+
+    handle_type handle_{invalid_handle};
+    
   public:
 
-    static constexpr auto std_error_handle = DWORD(-12);
-
     static std::unique_ptr<sink> open() noexcept {
-      return std::unique_ptr<sink>{new conerr{GetStdHandle(std_error_handle)}};
+#if defined(_WIN32)
+      return std::unique_ptr<sink>{new conerr{GetStdHandle(DWORD(-12))}};
+#elif defined(__linux__)
+        return std::unique_ptr<sink>{new conerr{2}};
+#endif
     }
 
     conerr() noexcept = default;
@@ -51,32 +72,40 @@ namespace chronicle::sinks {
     conerr& operator = (conerr const&) noexcept = delete;
 
     conerr(conerr&& other) noexcept: handle_{other.handle_} {
-      other.handle_ = nullptr;
+      other.handle_ = invalid_handle;
     }
 
     conerr& operator = (conerr&& other) noexcept {
-      handle_ = other.handle_; other.handle_ = nullptr;
+      handle_ = other.handle_; other.handle_ = invalid_handle;
       return *this;
     }
     
     
     bool ready() const noexcept override {
-      return handle_ != nullptr;
+      return handle_ != invalid_handle;
     }
 
 
     void write(time_point const&, char const* data, size_t size) noexcept override {
-      WriteFile(handle_, data, DWORD(size), nullptr, nullptr);
+#if defined(_WIN32)
+        WriteFile(handle_, data, DWORD(size), nullptr, nullptr);
+#elif defined(__linux__)
+        ::write(handle_, data, size);
+#endif
     }
     
     
     void flush() noexcept override {
-      FlushFileBuffers(handle_);
+#if defined(_WIN32)
+        FlushFileBuffers(handle_);
+#elif
+        ::fdatasync(handle_);
+#endif
     }
 
 
     void close() noexcept override {
-      handle_ = nullptr;
+      handle_ = invalid_handle;
     }
 
 
@@ -89,19 +118,10 @@ namespace chronicle::sinks {
 
 
   private:
-
-    void* handle_{nullptr};
-
-    conerr(void* handle) noexcept: handle_{handle} { }
+    
+    explicit conerr(handle_type handle) noexcept: handle_{handle} { }
 
   }; // conerr
 
 
-#else
-
-#error Unsupported system
-
-#endif
-
-
-} // chronicle::sinks
+} // namespace chronicle::sinks
