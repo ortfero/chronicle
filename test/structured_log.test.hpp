526089cd6a9c6c5a6a63847ff956320e612f2d99
ortfero <ortfero@gmail.com>
2022-03-28 15:33:45 +0300

    Add test suite for structured log tests

--- test/structured_log.test.hpp
+++ test/structured_log.test.hpp
@@ -5,18 +5,21 @@
 #include <chronicle/structured_log.hpp>
 #include <chronicle/sinks/conout.hpp>
 
+TEST_SUITE("structuted_log") {
 
-TEST_CASE("structured_log::structured_log") {
-    chronicle::shared_structured_log target;
-    REQUIRE(!target.opened());
-    target.info("test", "info", "name", "value");   // should be ok
-}
+    TEST_CASE("structured_log::structured_log") {
+        chronicle::shared_structured_log target;
+        REQUIRE(!target.opened());
+        target.info("test", "info", "name", "value");   // should be ok
+    }
 
 
-TEST_CASE("structured_log::attributes_log") {
-    chronicle::shared_structured_log target;
-    target.add_sink(chronicle::sinks::conout::open());
-    bool const opened = target.open();
-    REQUIRE(target.opened());
-    target.info("test", "info", "name", "value");
+    TEST_CASE("structured_log::attributes_log") {
+        chronicle::shared_structured_log target;
+        target.add_sink(chronicle::sinks::conout::open());
+        bool const opened = target.open();
+        REQUIRE(target.opened());
+        target.info("test", "info", "name", "value");
+    }
+
 }
