30993fc2104264bc6da28e8ce88317dc421e4efa
ortfero <ortfero@gmail.com>
2022-05-03 12:49:55 +0300

    Update hydra with futex_event fix for linux

--- include/hydra/futex_event.hpp
+++ include/hydra/futex_event.hpp
@@ -47,7 +47,7 @@ namespace hydra {
 
     class futex_event {
     private:
-        std::atomic_flag value_;
+        std::atomic_uint32_t value_;
 
     public:
         futex_event() noexcept = default;
@@ -55,11 +55,8 @@ namespace hydra {
         futex_event& operator=(futex_event const&) = delete;
 
 
-        void clear() noexcept { value_.clear(std::memory_order_relaxed); }
-
-
         void notify_one() noexcept {
-            value_.test_and_set(std::memory_order_relaxed);
+            value_.fetch_add(1, std::memory_order_relaxed);
 #if defined(_WIN32)
             WakeByAddressSingle(&value_);
 #elif defined(__linux__)
@@ -75,13 +72,14 @@ namespace hydra {
 
 
         void wait() noexcept {
-            auto undesired = false;
+            auto const wait_until_this = value_.load(std::memory_order_relaxed);
 #if defined(_WIN32)
             WaitOnAddress(&value_, &undesired, sizeof(value_), DWORD(-1));
 #elif defined(__linux__)
             syscall(SYS_futex,
+                    &value_,
                     FUTEX_WAIT_PRIVATE,
-                    undesired,
+                    wait_until_this,
                     nullptr,
                     nullptr,
                     0);
@@ -92,7 +90,7 @@ namespace hydra {
         template<typename Rep, typename Period>
         void wait(std::chrono::duration<Rep, Period> timeout) noexcept {
             using namespace std::chrono;
-            auto undesired = false;
+            auto const wait_until_this = value_.load(std::memory_order_relaxed);
 #if defined(_WIN32)
             auto const ms = duration_cast<milliseconds>(timeout);
             WaitOnAddress(&value_,
@@ -104,7 +102,13 @@ namespace hydra {
             auto const ns = duration_cast<nanoseconds>(timeout - secs);
             auto const ts =
                 timespec {std::time_t(secs.count()), long(ns.count())};
-            syscall(SYS_futex, FUTEX_WAIT_PRIVATE, undesired, &ts, nullptr, 0);
+            syscall(SYS_futex,
+                    &value_,
+                    FUTEX_WAIT_PRIVATE,
+                    wait_until_this,
+                    &ts,
+                    nullptr,
+                    0);
 #endif
         }
 
