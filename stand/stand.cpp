5361b565a7d099684fd957a01bfd5e07737a37ac
ortfero <ortfero@gmail.com>
2023-02-13 16:43:12 +0200

    Update stand with custom data log

--- stand/stand.cpp
+++ stand/stand.cpp
@@ -4,28 +4,64 @@
 #include <filesystem>
 #include <thread>
 
-#include <chronicle/structured_log.hpp>
+#include <chronicle/data_log.hpp>
 #include <chronicle/sinks/file.hpp>
+#include <chronicle/fields/severity_marker.hpp>
+
 #include <ufmt/print.hpp>
 
 
+enum class message_kind {
+    none, data
+}; // message_kind
+
+
+union message_data {
+    int value;
+    
+    message_data() { }
+    message_data(int v) noexcept: value{v} { }
+}; // message_data
+
+
+struct message {        
+    using kind_type = message_kind;
+
+    message_kind kind{message_kind::none};
+    message_data data;
+    
+    message() = default;
+    
+    message(int value) noexcept
+    : kind{message_kind::data}, data{value} { }
+    
+    template<typename S> friend S& operator << (S& stream, message const& m) {
+        switch(m.kind) {
+            case kind_type::data:
+                return stream << ' ' << '{' << m.data.value << '}';
+            default:
+                return stream << "#(unknown_message)";
+        }
+    }
+    
+}; // message
+
+using format = chronicle::fields::format<
+            chronicle::fields::utc_time_ms,
+            chronicle::fields::thread_id,
+            chronicle::fields::source>;
+
+using traits = chronicle::traits_shared<message, format, std::chrono::system_clock>;
+using logger = chronicle::data_log<traits>;
+
+
 int main(int, char**) {
-    auto log = chronicle::shared_structured_log{};
+    auto log = logger{4096};
     auto const opened = log.open(chronicle::sinks::file::open("test.log"));
     if(!opened)
-        return ufmt::error_with(-1, "Unable to open test log: ", opened.error().message());
-    auto done = std::atomic_bool{false};
-    auto logger = std::thread([&](){
-        while(!done) {
-            log.info("stand", "info");
-            std::this_thread::sleep_for(std::chrono::milliseconds{100});
-        }
-    });
-    ufmt::print("Press ENTER to stop");
-    std::getchar();
-    done = true;
-    logger.join();
-    log.close();
-    //std::filesystem::remove("test.log", ec);
+        return ufmt::error_with(-1, "error: ", opened.error().message());
+    
+    log.info("main", "sample", message{-1}); 
+    
     return 0;
 }
