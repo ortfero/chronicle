2fc968871d831e61b4c249c5f827ab26b04dcbd2
Andrei Ilin <ortfero@gmail.com>
2024-09-01 17:32:02 +0300

    Set severity marker as default

--- include/chronicle/fields/default_format.hpp
+++ include/chronicle/fields/default_format.hpp
@@ -6,7 +6,7 @@
 
 
 #include <chronicle/fields/format.hpp>
-#include <chronicle/fields/severity.hpp>
+#include <chronicle/fields/severity_marker.hpp>
 #include <chronicle/fields/source.hpp>
 #include <chronicle/fields/thread_id.hpp>
 #include <chronicle/fields/utc_time_ms.hpp>
@@ -19,21 +19,21 @@ namespace chronicle::fields {
 
 
     using format_multithreaded_us =
-        format<severity, utc_time_ms, thread_id, source>;
+        format<severity_marker, utc_time_ms, thread_id, source>;
     using format_multithreaded_ms =
-        format<severity, utc_time_ms, thread_id, source>;
+        format<severity_marker, utc_time_ms, thread_id, source>;
     using format_multithreaded_time_only_us =
-        format<severity, utc_time_only_us, thread_id, source>;
+        format<severity_marker, utc_time_only_us, thread_id, source>;
     using format_multithreaded_time_only_ms =
-        format<severity, utc_time_only_ms, thread_id, source>;
+        format<severity_marker, utc_time_only_ms, thread_id, source>;
 
-    using format_singlethreaded_us = format<severity, utc_time_us, source>;
-    using format_singlethreaded_ms = format<severity, utc_time_ms, source>;
+    using format_singlethreaded_us = format<severity_marker, utc_time_us, source>;
+    using format_singlethreaded_ms = format<severity_marker, utc_time_ms, source>;
 
     using format_singlethreaded_time_only_us =
-        format<severity, utc_time_only_us, source>;
+        format<severity_marker, utc_time_only_us, source>;
     using format_singlethreaded_time_only_ms =
-        format<severity, utc_time_only_ms, source>;
+        format<severity_marker, utc_time_only_ms, source>;
 
     using format_multithreaded_default = format_multithreaded_us;
     using format_singlethreaded_default = format_singlethreaded_us;
