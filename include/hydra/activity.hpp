23f78191eb041b0d8e74f43d72557d16dcc259fe
Andrei Ilin <ortfero@gmail.com>
2024-09-01 17:32:25 +0300

    Update hydra dependency

--- include/hydra/activity.hpp
+++ include/hydra/activity.hpp
@@ -2,12 +2,14 @@
 
 
 #include <atomic>
+#include <chrono>
 #include <memory>
 #include <thread>
+#include <utility>
 
 #include <hydra/batch.hpp>
-#include <hydra/futex_event.hpp>
 #include <hydra/mpsc_queue.hpp>
+#include <hydra/sequence.hpp>
 
 
 namespace hydra {
@@ -24,8 +26,7 @@ namespace hydra {
     private:
         std::thread worker_;
         queue_type messages_;
-        futex_event new_message_;
-        std::uint32_t messages_processed_ {0};
+        std::atomic_uint32_t new_message_;
         std::atomic_flag stopping_ {};
 
     public:
@@ -37,9 +38,12 @@ namespace hydra {
         sequence claim() noexcept { return messages_.claim(); }
         message_type& operator[](sequence n) noexcept { return messages_[n]; }
         void reserve(size_type n) noexcept { messages_.reserve(n); }
+
+        
         size_type blocks_count() const noexcept {
             return messages_.blocks_count();
         }
+
 
         template<typename Rep, typename Period>
         sequence claim_for(
@@ -50,14 +54,18 @@ namespace hydra {
 
         void publish(sequence n) noexcept {
             messages_.publish(n);
+            new_message_.fetch_add(1, std::memory_order_relaxed);
             new_message_.notify_one();
         }
 
 
         void stop() noexcept {
-            if(!worker_.joinable()
-               || stopping_.test_and_set(std::memory_order_relaxed))
+            auto const is_stopped_or_stopping = !worker_.joinable()
+                || stopping_.test(std::memory_order_relaxed);
+            if(is_stopped_or_stopping)
                 return;
+            stopping_.test_and_set(std::memory_order_relaxed);
+            new_message_.fetch_add(1, std::memory_order_relaxed);
             new_message_.notify_one();
             worker_.join();
         }
@@ -69,32 +77,27 @@ namespace hydra {
                 return false;
 
             worker_ = std::thread {[handler, this]() {
-                if(messages_.size() != 0) {
-                    auto messages = batch<Q> {messages_};
-                    handler(messages);
-                    messages_processed_ += messages.fetched_count();
+                while(!stopping_.test(std::memory_order_relaxed)) {
+                    new_message_.wait(0);
+                    process(handler);
                 }
-
-                while(!stopping_.test_and_set(std::memory_order_relaxed)) {
-                    stopping_.clear(std::memory_order_relaxed);
-                    new_message_.wait(messages_processed_);
-                    if(messages_.size() != 0) {
-                        auto messages = batch<Q> {messages_};
-                        handler(messages);
-                        messages_processed_ += messages.fetched_count();
-                    }
-                }
-
-                if(messages_.size() != 0) {
-                    auto messages = batch<Q> {messages_};
-                    handler(messages);
-                    messages_processed_ += messages.fetched_count();
-                }
-
+                process(handler);
+                new_message_.store(0, std::memory_order_relaxed);
                 stopping_.clear(std::memory_order_relaxed);
             }};
 
             return true;
+        }
+
+    private:
+
+        template<typename H>
+        void process(H&& handler) {
+            while(messages_.size() != 0) {
+                auto messages = batch_type{messages_};
+                handler(messages);
+                new_message_.fetch_sub(messages.fetched_count(), std::memory_order_relaxed);
+            }
         }
     };   // activity
 
